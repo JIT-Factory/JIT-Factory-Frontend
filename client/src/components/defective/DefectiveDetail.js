ffd67cc690fcac9e87bb75816a90ce8cc314aa1e
daeHui77 <104980470+znight1020@users.noreply.github.com>
2023-04-24 04:06:39 +0900

    feat: 불량품 로그 출력

--- client/src/components/defective/DefectiveDetail.js
+++ client/src/components/defective/DefectiveDetail.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import * as React from "react";
+import { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import axios from "axios";
 
-function DefectiveDetail() {
-  return (
-    <div>DefectiveDetail</div>
-  )
+// Generate user Data
+function createData(id, date, category, productId, reason) {
+  const formattedDate = new Date(date).toISOString().slice(0, 10);
+  return { id, date: formattedDate, category, productId, reason };
 }
 
-export default DefectiveDetail
+export default function DefectiveDetail() {
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("/api/product/status/fail")
+            .then((response) => {
+                const data = response.data
+                    .map((product) =>
+                        createData(
+                          product.id,
+                          product.createTime,
+                          product.productName,
+                          product.id,
+                          product.reason,
+                        )
+                    )
+                    .sort((a, b) => {
+                        if (a.role === "ADMIN" && b.role !== "ADMIN") {
+                            return -1;
+                        } else if (a.role !== "ADMIN" && b.role === "ADMIN") {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    });
+                setRows(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <React.Fragment>
+            <h2 style={{ paddingBottom: "1vmax" }}>JIT Factory 불량품 현황</h2>
+            <Table size="medium">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            <h2>날짜</h2>
+                        </TableCell>
+                        <TableCell>
+                            <h2>카테고리</h2>
+                        </TableCell>
+                        <TableCell>
+                            <h2>상품 ID</h2>
+                        </TableCell>
+                        <TableCell>
+                            <h2>내용</h2>
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.id}>
+                            <TableCell>{row.date}</TableCell>
+                            <TableCell>{row.category}</TableCell>
+                            <TableCell>{row.productId}</TableCell>
+                            <TableCell>{row.reason}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </React.Fragment>
+    );
+}
