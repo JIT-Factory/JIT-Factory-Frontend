2140de0c18377142733ceb8be9d0a11107814536
현수 <znight1020@naver.com>
2023-04-25 06:15:27 +0900

    feat: Dashboard 불량품 로그를 5개까지만 노출

--- client/src/components/dashboard/Defective.js
+++ client/src/components/dashboard/Defective.js
@@ -1,32 +1,59 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import Link from "@mui/material/Link";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import axios from "axios";
 
 // Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-    return { id, date, name, shipTo, paymentMethod, amount };
+function createData(id, date, category, productId, reason) {
+    const formattedDate = new Date(date).toISOString().slice(0, 10);
+    return { id, date: formattedDate, category, productId, reason };
 }
-
-const rows = [
-    createData(0, "2023년 1월 1일", "작물", "1", "곰팡이"),
-    createData(1, "2023년 1월 1일", "작물", "2", "유통기한"),
-    createData(2, "2023년 1월 1일", "작물", "3", "잘못된 상품 이름"),
-    createData(3, "2023년 1월 1일", "작물", "4", "등"),
-    createData(4, "2023년 1월 1일", "작물", "5", "등"),
-];
 
 function preventDefault(event) {
     event.preventDefault();
 }
 
 export default function Orders() {
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("/api/product/status/fail")
+            .then((response) => {
+                const data = response.data
+                    .map((product) =>
+                        createData(
+                            product.id,
+                            product.createTime,
+                            product.productName,
+                            product.id,
+                            product.reason
+                        )
+                    )
+                    .sort((a, b) => {
+                        if (a.role === "ADMIN" && b.role !== "ADMIN") {
+                            return -1;
+                        } else if (a.role !== "ADMIN" && b.role === "ADMIN") {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    })
+                    .slice(-5); // 최근 5개 데이터만 남깁니다.
+                setRows(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
     return (
         <React.Fragment>
-            <h2>불량품 로그</h2>
+            <h2>최근 불량품 로그</h2>
             <Table size="medium">
                 <TableHead>
                     <TableRow>
@@ -48,9 +75,9 @@ export default function Orders() {
                     {rows.map((row) => (
                         <TableRow key={row.id}>
                             <TableCell>{row.date}</TableCell>
-                            <TableCell>{row.name}</TableCell>
-                            <TableCell>{row.shipTo}</TableCell>
-                            <TableCell>{row.paymentMethod}</TableCell>
+                            <TableCell>{row.category}</TableCell>
+                            <TableCell>{row.productId}</TableCell>
+                            <TableCell>{row.reason}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
